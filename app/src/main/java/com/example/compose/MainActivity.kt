bba4a177449c35f1ccbdc89e54044d997acd5bb2
sunyoung <youngsjungg@gmail.com>
2022-09-18 23:20:20 +0900

    Build:초기설정

---
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard("Android")
+        }
+    }
+}
+@Composable
+fun MessageCard(name : String) {
+    Text(text = "Hello $name !")
+}
+@Preview
+@Composable
+fun PreviewMessgeCard() {
+    MessageCard("Android ")
+}
